0bc2c86eda62f260fb2640a7db06ada619af5ff8
daidai411411 <daidai411411@gmail.com>
2025-04-04 03:19:16 +0900

    fix: クエリ文字列があっても機能するように修正

--- content.js
+++ content.js
@@ -1,5 +1,7 @@
 document.addEventListener("click", function (event) {
-	// "学認でログイン" ボタンの要素を取得
-	let loginBtn = document.querySelector('[href="https://cclms.kyoto-su.ac.jp/auth/shibboleth/index.php"');
-	loginBtn.click();
+  // "学認でログイン" ボタンの要素を取得
+  let loginBtn = document.querySelector(
+    '[href="https://cclms.kyoto-su.ac.jp/auth/shibboleth/index.php"'
+  );
+  loginBtn.click();
 });
